e0015f397c567978ea2260cf5328ad751809f13d
tekkub <tekkub@gmail.com>
2010-01-16 00:47:50 -0700

    Make clicking open a tab

--- src/background.js
+++ src/background.js
@@ -5,11 +5,28 @@ function updatebadge() {
   })
 }
 
+function inbox() {
+  chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    var found = false
+    $.each(tabs, function (i, tab) {
+      if (tab.url && (/\/\/support\.github\.com\//).test(tab.url)) {
+        chrome.tabs.update(tab.id, {selected: true});
+        found = true;
+        return false;
+      }
+    })
+    if (!found) {chrome.tabs.create({url: "http://support.github.com/"})}
+  });
+}
+
 $(function() {
   $.ajaxSetup({"beforeSend": function(xhr) {
       xhr.setRequestHeader("Accept", "application/vnd.tender-v1+json");
       xhr.setRequestHeader("X-Tender-Auth", "6efdae1ad9e459d218a33875ab9369270b620612");
   }});
+
   setInterval(updatebadge,"60000");
   updatebadge();
+
+  chrome.browserAction.onClicked.addListener(function(tab) {inbox()});
 });
